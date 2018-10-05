12222a1a1c657add359e1b7c132856e6162359f8
danieljgraves <daniel.graves@rig.net>
2018-10-05 11:57:19 -0500

    Code to deploy EKS cluster in AWS

---
+++ output.tf
@@ -0,0 +1,30 @@
+locals {
+  config-map-aws-auth = <<CONFIGMAPAWSAUTH
+
+
+apiVersion: v1
+kind: ConfigMap
+metadata:
+  name: aws-auth
+  namespace: kube-system
+data:
+  mapRoles: |
+    - rolearn: ${aws_iam_role.demo-node.arn}
+      username: system:node:{{EC2PrivateDNSName}}
+      groups:
+        - system:bootstrappers
+        - system:nodes
+CONFIGMAPAWSAUTH
+}
+
+output "config-map-aws-auth" {
+  value = "${local.config-map-aws-auth}"
+}
+
+output "endpoint" {
+  value = "${aws_eks_cluster.demo.endpoint}"
+}
+
+output "kubeconfig-certificate-authority-data" {
+  value = "${aws_eks_cluster.demo.certificate_authority.0.data}"
+}
---
+++ var.tf
@@ -0,0 +1,8 @@
+variable "cluster-name" {
+  default = "dgraves-eks-test"
+  type    = "string"
+}
+
+variable "access_key" {}
+
+variable "secret_key" {}
